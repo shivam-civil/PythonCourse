132740fea942d9f6187014eae93d5f17626a20d8
idocollab <ryuzaki.till@gmail.com>
2025-12-19 17:52:39 +0545

    Adding __getitem__ and __setitem__

--- DunderMethods/main.py
+++ DunderMethods/main.py
@@ -7,7 +7,7 @@ class Beam:
 """        
 
 
-#Exercise of __str__(self)
+#Exercise of __str__(self)   for Human Reading
 """
 class Beam:
     def __init__(self,length):
@@ -18,7 +18,7 @@ obj=Beam(10)
 print(obj)  # for __str__(self)
 """      
 
-#Exercise of __repr__(self)
+#Exercise of __repr__(self)    For Developer's Reading
 """
 class Beam:
     def __init__(self,length):
@@ -32,7 +32,7 @@ print([obj])   #for __repr__
 """
 
 
-#Exercise of __len__()
+#Exercise of __len__(self)
 """
 class SiteTasks:
     def __init__(self,tasks):
@@ -43,4 +43,37 @@ obj=SiteTasks(["BeamCast","ColumnCast","Excavation","Curing"])
 print(len(obj))     
 """   
 
-#Exercise of 
+#Exercise of __getitem__(self)
+"""
+class Beam:
+    def __init__(self,details):
+        self.details=details
+    def __getitem__(self,index):
+        return self.details[index]
+obj=Beam(["beam1","20*20",45])
+print(obj[0]) 
+"""       
+
+#Exercise of __setitem__(self)
+"""
+class Beam:
+    def __init__(self,details):
+        self.details=details
+    def __getitem__(self,index):
+        return self.details[index]
+    def __setitem__(self,key,value):
+        self.details[key]=value
+    def __str__(self):
+        return str(self.details) 
+    def __len__(self):
+        return len(self.details)       
+
+obj=Beam(["beam1","20*20",45])
+obj[0]="Beam2"
+print(obj)
+"""
+
+
+
+
+
